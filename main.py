1766b5688be1085a2cf29788ea843f5302d10cf2
Vedansh Ruhela <86541021+vruhela@users.noreply.github.com>
2022-01-13 10:47:55 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def transform(b):
+    for i in range(len(b)-1):
+        if b[i] == "1":
+            b[i] = "0"
+            if b[i+1] == "0":
+                b[i+1] == "1"
+            else:
+                b[i+1] == "0"
+    return b 
+
+if __name__ == "__main__":
+    a = list(input("Please enter a binary number to be terminated : "))
+    print(a)
+    while a != transform(a.copy()):
+        a = transform(a.copy())
+    print(a)    
